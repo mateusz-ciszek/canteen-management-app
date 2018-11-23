97cabb6695d3d1d5bb133f2f3714a7bda8c46e5e
Mateusz Ciszek <cichu32@gmail.com>
2018-11-23 19:59:18 +0100

    Wejście na bazowy adres przekirowuje do /main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LoginComponent } from "./login/login.component";
-import { MainComponent } from "./main/main.component";
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'main', component: MainComponent },
+  { path: '**', redirectTo: 'main' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
