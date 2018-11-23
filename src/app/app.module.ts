42e3babf626db52e9b2a86aa973695521ebd3d18
Mateusz Ciszek <cichu32@gmail.com>
2018-11-23 19:53:42 +0100

    Dodany wstępny layout panelu logowania

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MainModule } from './main/main.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,8 +16,9 @@ import { MainModule } from './main/main.module';
     BrowserModule,
     AppRoutingModule,
     MainModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.less']
+  styleUrls: ['./login.component.less'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onLoginClick() {
+    console.log(`Email: ${this.email}`);
+    console.log(`Password: ${this.password}`);
   }
 
 }
