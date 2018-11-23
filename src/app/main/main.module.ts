ac4352a3eccb771d6bc331535f72ef561157dad6
Mateusz Ciszek <cichu32@gmail.com>
2018-11-23 16:57:50 +0100

    Wygenerowanie komponentu dla głównego modułu

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainComponent } from './main.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [MainComponent],
   imports: [
     CommonModule
   ]
