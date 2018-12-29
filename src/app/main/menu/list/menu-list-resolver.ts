a180498f3417f40073d19ee321f9c2b98af2a378
Mateusz Ciszek <cichu32@gmail.com>
2018-12-29 18:17:53 +0100

    Moved fetching menus from resolver to separate service

--- src/app/main/menu/list/menu-list-resolver.ts
+++ src/app/main/menu/list/menu-list-resolver.ts
@@ -1,17 +1,16 @@
-import { HttpClient } from '@angular/common/http';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Menu } from '../../../models';
+import { MenuService } from '../../../services/menu.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MenuListResolver implements Resolve<Menu[]> {
-  constructor(private http: HttpClient) {}
+  constructor(private menuService: MenuService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Menu[]> | Promise<Menu[]> | Menu[] {
-    const url: string = '/menu';
-    return this.http.get<Menu[]>(url);
+    return this.menuService.getMenuList();
   }
 }
