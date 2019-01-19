ba0c21971a7265af1876eb754b5ac1701e8a4276
Mateusz Ciszek <202132@edu.p.lodz.pl>
2019-01-19 14:01:03 +0100

    User selected addition have their prices next to them on order details

--- src/app/common/pipe/additions-formatter.pipe.ts
+++ src/app/common/pipe/additions-formatter.pipe.ts
@@ -1,11 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { OrderItemAddition } from '../../models';
+import { CurrencyFormatterPipe } from './currency-formatter.pipe';
 
 @Pipe({
-  name: 'additionsFormatter'
+  name: 'additionsFormatter',
 })
 export class AdditionsFormatterPipe implements PipeTransform {
   transform(values: OrderItemAddition[]): string {
-    return values.length ? values.map(item => item.foodAddition.name).join(', ') : '-';
+    const currencyPipe: CurrencyFormatterPipe = new CurrencyFormatterPipe();
+    const namesWithPrice: string[] = values.map(item => `${item.foodAddition.name} (${currencyPipe.transform(item.price)})`);
+    return values.length ? namesWithPrice.join(', ') : '-';
   }
 }
