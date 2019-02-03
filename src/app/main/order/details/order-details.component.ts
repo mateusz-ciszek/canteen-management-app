01bd42878cbf02fa66289581e0442fd98a0ddeaa
Mateusz Ciszek <202132@edu.p.lodz.pl>
2019-02-03 14:49:39 +0100

    Showing comment on order details

--- src/app/main/order/details/order-details.component.ts
+++ src/app/main/order/details/order-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Order } from '../../../models';
 
@@ -7,16 +7,27 @@ import { Order } from '../../../models';
   templateUrl: './order-details.component.html',
   styleUrls: ['./order-details.component.less']
 })
-export class OrderDetailsComponent {
+export class OrderDetailsComponent implements OnInit {
 
   order: Order;
+  expanded: boolean;
+  hasComment: boolean;
 
   constructor(private route: ActivatedRoute) {
     this.order = route.snapshot.data['order'];
   }
 
+  ngOnInit(): void {
+    this.hasComment = this.order.comment && this.order.comment !== '';
+    this.expanded = this.hasComment;
+  }
+
   contact() {
     location.href = `mailto:${this.order.user.email}`;
+  }
+
+  changeCommentVisibility() {
+    this.expanded = !this.expanded;
   }
 
 }
