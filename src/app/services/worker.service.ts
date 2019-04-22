24abf669132729fcf4652613b206b2aea5dd095a
Mateusz Ciszek <202132@edu.p.lodz.pl>
2019-04-22 19:20:05 +0200

    Added ability to approve or reject day off request

--- src/app/services/worker.service.ts
+++ src/app/services/worker.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { MonthResponse } from '../models';
+import { DayOffState, MonthResponse } from '../models';
 
 @Injectable({
   providedIn: 'root',
@@ -17,5 +17,10 @@ export class WorkerService {
   requestDaysOff(dates: Date[]): Observable<void> {
     const request = { dates: dates.map(date => date.toISOString()) };
     return this.http.post<void>('/worker/dayOff', request);
+  }
+
+  changeDayOffState(id: string, state: DayOffState): Observable<void> {
+    const request = { id, state };
+    return this.http.patch<void>('/worker/dayoff', request);
   }
 }
